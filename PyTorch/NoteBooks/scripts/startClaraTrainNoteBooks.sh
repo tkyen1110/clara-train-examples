deecbd2795283cc406d45e1c8123db32221a3f6a
TK.Yen <tk.yen@advantech.com.tw>
2021-08-23 16:31:03 +0800

    Revise the docker environment

--- PyTorch/NoteBooks/scripts/startClaraTrainNoteBooks.sh
+++ PyTorch/NoteBooks/scripts/startClaraTrainNoteBooks.sh
@@ -2,13 +2,16 @@
 
 # SPDX-License-Identifier: Apache-2.0
 
-./stopClaraTrainNoteBooks.sh
+bash ./stopClaraTrainNoteBooks.sh
 
 for i in {1..2}
 do
    echo ------------------------------------------------
 done
 
+export UID=$UID
+export GID=$(id -g)
+export USER=$USER
 #docker-compose build --no-cache
 docker-compose -p claratrain-triton up --remove-orphans -d
 
